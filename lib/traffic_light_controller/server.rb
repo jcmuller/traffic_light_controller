e6d58632b9bd787bd659ab99eadba2991ec96dff
Juan C. Müller <jcmuller@gmail.com>
2012-11-12 15:02:22 -0500

    Refactor and test

--- lib/traffic_light_controller/server.rb
+++ lib/traffic_light_controller/server.rb
@@ -6,53 +6,97 @@ module TrafficLightController
       @config = Config.new
       @server = TCPServer.new(config.server.address, config.server.port)
     rescue Errno::EADDRINUSE
-      puts "Address in use"
+      address_in_use_error(config.server)
     rescue Errno::EADDRNOTAVAIL, SocketError
-      puts "Address not available"
+      address_not_available_error(config.server.address)
     end
 
-    def work(run_forever = true)
-      while(run_forever)
-        Thread.start(server.accept) do |client|
-          begin
-            path = nil
-
-            while(line = client.readline) do
-              break if line == "\r\n"
-              if match = line.match(%r{GET /+(?<path>\w*) HTTP/1\.})
-                path = match[:path]
-              end
-            end
-
-            if config.arduino.lights.has_key?(path)
-              process(path)
-              client.print "HTTP/1.1 200 OK\r\nContent-type:text/plain\r\n\r\n"
-              client.puts path
-            else
-              client.print "HTTP/1.1 404 Not Found\r\nContent-type:text/plain\r\n\r\n"
-              client.puts "The requested path doesn't exist"
-            end
-          rescue => e
-            puts "Got #{e}!"
-          ensure
-            client.close
-          end
-        end
+    def work
+      while(true)
+        start_thread_and_do_work
       end
     end
 
-    def process(path)
+    private
+
+    attr_reader :server, :config, :board
+
+    def start_thread_and_do_work
+      Thread.start(server.accept) do |client|
+        work_with_client(client)
+      end
+    end
+
+    def work_with_client(client)
+      puts "work_with_client"
+      process_path(process_request(client))
+    rescue => e
+      puts "Got #{e.class}: #{e}!"
+    ensure
+      client.close
+    end
+
+    def process_request(client)
+      puts "process_request"
+      path = nil
+
+      while(line = client.readline) do
+        break if line == "\r\n"
+        if match = line.match(%r{GET /+(?<path>\w*) HTTP/1\.})
+          path = match[:path]
+        end
+      end
+
+      path
+    end
+
+    def process_path(path)
+      puts "process_path"
+      if config.arduino.lights.has_key?(path)
+        change_pins(path)
+        client.print "HTTP/1.1 200 OK\r\nContent-type:text/plain\r\n\r\n"
+        client.puts path
+      else
+        client.print "HTTP/1.1 404 Not Found\r\nContent-type:text/plain\r\n\r\n"
+        client.puts "The requested path doesn't exist"
+      end
+    end
+
+    def change_pins(path)
+      puts "change_pins"
+      init_board
       board.turnOff
       board.setHigh(config.arduino.lights[path]) unless path == "off"
       board.close
     end
 
-    private
-
-    attr_reader :server, :config
-
-    def board
+    def init_board
+      puts "board"
       @board = Arduino.new(config.arduino.port)
+    rescue Errno::ENOENT
+      STDERR.puts "The port #{config.arduino.port} doesn't exist"
+      exit
+    rescue ArgumentError
+      STDERR.puts "#{config.arduino.port} is not a serial port"
+      exit
+    end
+
+    # ERRORS
+    def address_in_use_error(server)
+      STDERR.puts <<-EOT
+There appears that another instance is running, or another process
+is listening on the same port (#{server.address}:#{server.port})
+
+      EOT
+      exit
+    end
+
+    def address_not_available_error(address)
+      STDERR.puts <<-EOT
+The address configured is not available (#{address})
+
+      EOT
+      exit
     end
   end
 end
--- spec/lib/traffic_light_controller/server_spec.rb
+++ spec/lib/traffic_light_controller/server_spec.rb
@@ -1,4 +1,96 @@
 require 'spec_helper'
 
 describe TrafficLightController::Server do
+  let(:board) { mock }
+  let(:config) { mock }
+  let(:server) { mock(accept: client) }
+  let(:client) { mock }
+
+  before do
+    config.stub_chain(:server, :address).and_return("127.0.0.1")
+    config.stub_chain(:server, :port).and_return(10000 + rand(1000))
+    config.stub_chain(:arduino, :port).and_return("port")
+
+    TrafficLightController::Config.stub(:new).and_return(config)
+    TCPServer.stub(:new).and_return(server)
+    STDOUT.stub(:puts)
+  end
+
+  context "public methods" do
+    before do
+      subject.stub(:board).and_return(board)
+    end
+
+    describe "#initialize" do
+      it "should call address_in_use_error if address is in use" do
+        TCPServer.should_receive(:new).and_raise(Errno::EADDRINUSE)
+        described_class.any_instance.should_receive(:address_in_use_error)
+        described_class.new
+      end
+
+      it "should call address_in_use_error if address is not available" do
+        TCPServer.should_receive(:new).and_raise(Errno::EADDRNOTAVAIL)
+        described_class.any_instance.should_receive(:address_not_available_error)
+        described_class.new
+      end
+    end
+
+    describe "#change_pins" do
+      before { subject.stub(:init_board) }
+
+      it "set high the pin pointed to by path" do
+        config.stub_chain(:arduino, :lights).and_return({"blah" => 4})
+        board.should_receive(:turnOff)
+        board.should_receive(:setHigh).with(4)
+        board.should_receive(:close)
+
+        subject.send(:change_pins, "blah")
+      end
+
+      it "should not set high any pins" do
+        board.should_receive(:turnOff)
+        board.should_not_receive(:setHigh)
+        board.should_receive(:close)
+
+        subject.send(:change_pins, "off")
+      end
+    end
+  end
+
+  context "private methods" do
+    describe "#board" do
+      it "should instantiate a new arduino object" do
+        Arduino.should_receive(:new).with("port")
+        subject.send(:init_board)
+      end
+    end
+  end
+
+  describe "#start_thread_and_do_work" do
+    it "should start a thread" do
+      Thread.should_receive(:start).and_yield(client)
+      subject.should_receive(:work_with_client).with(client)
+      subject.send(:start_thread_and_do_work)
+    end
+  end
+
+  describe "#work_with_client" do
+    it "should call process path" do
+      subject.should_receive(:process_request).with(client).and_return(:processed)
+      subject.should_receive(:process_path).with(:processed)
+      client.should_receive(:close)
+
+      subject.send(:work_with_client, client)
+    end
+
+    it "should rescue from all exceptions and log them to STDOUT" do
+      subject.should_receive(:process_request).and_raise RuntimeError
+
+      STDOUT.should_receive(:puts).with("Got RuntimeError: RuntimeError!")
+
+      client.should_receive(:close)
+
+      subject.send(:work_with_client, client)
+    end
+  end
 end
